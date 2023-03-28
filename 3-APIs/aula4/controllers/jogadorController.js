53838d719a6269528ffa8eaf7b3cbbc95de65c61
ThorNoSenac <gallithor@gmail.com>
2023-03-28 11:24:53 -0300

    Add search by name, remove total and serach by id

--- 3-APIs/aula4/controllers/jogadorController.js
+++ 3-APIs/aula4/controllers/jogadorController.js
@@ -1,23 +1,15 @@
-import { sequelize } from "../databases/conecta.js";
 import { Jogadores } from "../models/Jogador.js";
+import { Op } from "sequelize";
 
-export const jogadorRead = async (req, res) => {
-    try {
-        const jogadores = await Jogadores.findAll();
-        res.status(200).json(jogadores);
-    } catch (error) {
-        res.status(400).send(error);
-    }
-};
-
+// <CRUD>
 export const jogadorCreate = async (req, res) => {
-    const { nome, clube, posicao, idade, salário } = req.body;
+    const { nome, clube, posicao, idade, salario } = req.body;
 
     // se não informou estes atributos
-    if (!nome || !clube || !posicao || !idade || !salário) {
+    if (!nome || !clube || !posicao || !idade || !salario) {
         res.status(400).json({
             id: 0,
-            msg: "Erro... Informe nome, clube, posicao, idade e salário do jogador.",
+            msg: "Erro... Informe nome, clube, posicao, idade e salario do jogador.",
         });
         return;
     }
@@ -28,7 +20,7 @@ export const jogadorCreate = async (req, res) => {
             clube,
             posicao,
             idade,
-            salário,
+            salario,
         });
         res.status(201).json(jogador);
     } catch (error) {
@@ -36,14 +28,23 @@ export const jogadorCreate = async (req, res) => {
     }
 };
 
+export const jogadorRead = async (req, res) => {
+    try {
+        const jogadores = await Jogadores.findAll();
+        res.status(200).json(jogadores);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
 export const jogadorUpdate = async (req, res) => {
     const { id } = req.params;
-    const { nome, clube, posicao, idade, salário } = req.body;
+    const { nome, clube, posicao, idade, salario } = req.body;
 
-    if (!nome || !clube || !posicao || !idade || !salário) {
+    if (!nome || !clube || !posicao || !idade || !salario) {
         res.status(400).json({
             id: 0,
-            msg: "Erro... Informe nome, clube, posicao, idade e salário do jogador.",
+            msg: "Erro... Informe nome, clube, posicao, idade e salario do jogador.",
         });
         return;
     }
@@ -55,7 +56,7 @@ export const jogadorUpdate = async (req, res) => {
                 clube,
                 posicao,
                 idade,
-                salário,
+                salario,
             },
             {
                 where: { id },
@@ -73,40 +74,25 @@ export const jogadorDelete = async (req, res) => {
         const jogador = await Jogadores.destroy({
             where: { id },
         });
-        res.status(200).json(jogador);
+        res.status(200).json({ id, msg: "Jogador removido" });
     } catch (error) {
         res.status(400).send(error);
     }
 };
+// </CRUD>
 
+// <Filters>
 export const jogadorSearch = async (req, res) => {
-    const { id } = req.params;
+    const { pesquisa } = req.body;
     try {
-        const jogador = await Jogadores.findByPk(id);
-        if (jogador) {
-            res.status(200).json(jogador);
+        const jogadores = await Jogadores.findAll({
+            where: { nome: { [Op.like]: `%${pesquisa}%` } },
+        });
+        if (jogadores?.length > 0) {
+            res.status(200).json({ jogadores });
         } else {
             res.status(200).json({
-                id: 0,
-                msg: "Erro... Jogador não encontrado.",
-            });
-        }
-    } catch (error) {
-        res.status(400).send(error);
-    }
-};
-
-export const jogadorTotal = async (req, res) => {
-    try {
-        const num = await Jogadores.count();
-        const total = await Jogadores.sum("salário");
-
-        if (num && total) {
-            res.status(200).json({ num, total });
-        } else {
-            res.status(200).json({
-                id: 0,
-                msg: "Erro... Falha no cálculo dos totais",
+                msg: "Nenhum resultado encontrado.",
             });
         }
     } catch (error) {
--- 3-APIs/aula4/routes.js
+++ 3-APIs/aula4/routes.js
@@ -4,7 +4,6 @@ import {
     jogadorDelete,
     jogadorRead,
     jogadorSearch,
-    jogadorTotal,
     jogadorUpdate,
 } from "./controllers/jogadorController.js";
 
@@ -15,7 +14,6 @@ router
     .post("/jogadores", jogadorCreate)
     .put("/jogadores/:id", jogadorUpdate)
     .delete("/jogadores/:id", jogadorDelete)
-    .get("/jogadores/total", jogadorTotal)
-    .get("/jogadores/:id", jogadorSearch);
+    .get("/jogadores/search", jogadorSearch);
 
 export default router;
