30553f8061ccef01d769ae2aced5f87a1a81905a
ThorNoSenac <gallithor@gmail.com>
2023-04-28 11:59:16 -0300

    :construction: Add win condition, game info and comments

--- 3sem/2-Algoritmos/mine_sweeper/sweepy.py
+++ 3sem/2-Algoritmos/mine_sweeper/sweepy.py
@@ -8,6 +8,12 @@ mark_char = "!"
 padding = "   "
 highscores = []
 
+MAP_SMALL = {"name": "Pequeno", "size": 6, "bomb_amount": 5}
+MAP_MEDIUM = {"name": "Médio", "size": 9, "bomb_amount": 12}
+MAP_LARGE = {"name": "Grande", "size": 12, "bomb_amount": 20}
+
+active_maps = [MAP_SMALL, MAP_MEDIUM, MAP_LARGE]
+
 
 def read_file():
     if not os.path.isfile("highscores.csv"):
@@ -38,12 +44,14 @@ def print_separator(size=32, char="="):
 
 # Menu de seleção de tamanho do campo
 def choose_size():
+    print()
     print_separator()
     print(f"{'':^7}Tamanho do campo")
-    print_separator()
-    print(f"{'':^5}1. Pequeno (8x8)")
-    print(f"{'':^5}2. Médio (12x12)")
-    print(f"{'':^5}3. Grande (16x16)")
+    print_separator(size=16, char=' -')
+
+    print(f"{'':^5}1. Pequeno ({MAP_SMALL['size']}x{MAP_SMALL['size']})")
+    print(f"{'':^5}2. Médio ({MAP_MEDIUM['size']}x{MAP_MEDIUM['size']})")
+    print(f"{'':^5}3. Grande ({MAP_LARGE['size']}x{MAP_LARGE['size']})")
     print()
     print(f"{'':^5}0. Cancelar")
     print_separator()
@@ -51,19 +59,19 @@ def choose_size():
     choice = input("\nDigite sua escolha: ")
 
     if choice == "1":
-        size, bombs = 8, 10
+        map_selected = MAP_SMALL
     elif choice == "2":
-        size, bombs = 12, 25
+        map_selected = MAP_MEDIUM
     elif choice == "3":
-        size, bombs = 16, 50
+        map_selected = MAP_LARGE
     elif choice == "0":
-        return None, None
+        return None
     else:
         print_invalid_input()
         # Chama a função novamente para que o usuário escolha uma opção válida
         return choose_size()
 
-    return size, bombs
+    return map_selected
 
 
 # Retorna um mapa de preenchido com o caractere especificado
@@ -128,30 +136,26 @@ def print_coordinates(size):
 
 def print_map(map):
     size = len(map)
-    # Calculate the size of the game title based on the map size
     title_size = (size * 4 + 4 - len(title)) // 2
 
-    # On the first row: print the game title and the coordinates
+    # Imprime o  título e o topo do tabuleiro
     print(" " * title_size + title)
     print_coordinates(size)
     print(padding + "┌" + "───┬" * (size - 1) + "───┐")
 
+    # Imprime as linhas do tabuleiro
     for row in range(size):
+        print(f"{row + 1:2} ", end="")
 
-        # Print the left border of the row
-        print(f"{row+1:2} ", end="")
-
-        # Print the contents of the row
         for col in range(size):
             print(f"│ {map[row][col]} ", end="")
 
-        # Print the right border of the row and the chess-like coordinates
-        print(f"│ {row+1:2}")
+        print(f"│ {row + 1:2}")
 
-        # On the last row: print the bottom border of the map
         if row != size - 1:
-            print(padding+"├" + "───┼" * (size - 1) + "───┤")
+            print(padding + "├" + "───┼" * (size - 1) + "───┤")
 
+    # Imprime a base do tabuleiro
     print(padding + "└" + "───┴" * (size - 1) + "───┘")
     print_coordinates(size)
 
@@ -166,36 +170,32 @@ def validate_input(player_input, valid_coords):
 
     # Um input válido deve ter pelo menos 2 caracteres
     if len(coords) < 2:
-        return False, None, None
+        return None, None
 
     # O primeiro caractere deve ser uma letra, o resto deve ser um número
     letter = str(coords[0]).upper()
     number = coords[1:]
 
-    # Verifica se a letra
-    if letter not in valid_coords:
-        return False, None, None
-
-    # Verifica se o número é um inteiro válido
+    # Verifica se o resto do input é um número inteiro
     try:
         number = int(number)
     except ValueError:
-        return False, None, None
+        return None, None
 
-    # Verifica se o número está dentro do range válido
+    # Verifica se o número está dentro do intervalo válido
     if number < 1 or number > len(valid_coords):
-        return False, None, None
+        return None, None
+    # Verifica se a letra está dentro do intervalo válido
+    if letter not in valid_coords:
+        return None, None
 
     # Se o input for válido, retorna as coordenadas e o comando
     coord_values = (number - 1, valid_coords.index(letter))
-    return True, coord_values, command
-
-
-def get_tile(map, coords):
-    return map[coords[0]][coords[1]]
-
+    return coord_values, command
 
 # Utilização de recursão para revelar os tiles ao redor caso o tile atual seja zero
+
+
 def reveal_tiles_around_zeros(player_map, game_map, coord):
     size = len(game_map)
     row, col = coord
@@ -217,81 +217,154 @@ def reveal_tiles_around_zeros(player_map, game_map, coord):
     return player_map
 
 
+def count_revealed(_map):
+    count = 0
+    size = len(_map)
+    for row in range(size):
+        for col in range(size):
+            tile = _map[row][col]
+            if tile != empty_char and tile != mark_char:
+                count += 1
+    return count
+
+
+def count_marked(_map):
+    count = 0
+    size = len(_map)
+    for row in range(size):
+        for col in range(size):
+            tile = _map[row][col]
+            if tile == mark_char:
+                count += 1
+    return count
+
+
+def game_over(game_map):
+    print("  ____     ____     ____    __  __   _ ")
+    print(" |  _ \\   / __ \\   / __ \\  |  \\/  | | |")
+    print(" | |_) | | |  | | | |  | | | \\  / | | |")
+    print(" |  _ <  | |  | | | |  | | | |\\/| | | |")
+    print(" | |_) | | |__| | | |__| | | |  | | |_|")
+    print(" |____/   \\____/   \\____/  |_|  |_| (_)")
+    print("\nVocê perdeu! Cheque o mapa revelado!\n")
+    print_map(game_map)
+    input("\nPressione [Enter] para voltar ao menu.\n")
+
+
+def game_win(size):
+    print(" __          __  _____   _   _   _ ")
+    print(" \\ \\        / / |_   _| | \\ | | | |")
+    print("  \\ \\  /\\  / /    | |   |  \\| | | |")
+    print("   \\ \\/  \\/ /     | |   | . ` | | |")
+    print("    \\  /\\  /     _| |_  | |\\  | |_|")
+    print("     \\/  \\/     |_____| |_| \\_| (_)")
+    print("\nVocê venceu!\n")
+    player_name = input("Insira seu nome para registrar sua vitória: ")
+    print("TODO: Mostrar colocação")
+    input("\nPressione [Enter] para voltar ao menu.\n")
+
+
 # Game loop principal de uma partida
-def game_loop(player_map, game_map):
+def game_loop(player_map, game_map, bomb_amount):
     # Variáveis para controle do jogo
     size = len(game_map)
+    tile_amount = size*size
+    safe_amount = tile_amount - bomb_amount
     valid_coords = []
     for i in range(size):
         valid_coords.append(chr(i + ord('A')))
 
-    print_map(game_map)  # Debug, TODO: remover depois
-
     # Loop principal do jogo
     while True:
         # Mostra o mapa e instruções para o jogador
+        revealed_amount = count_revealed(player_map)
+        marked_amount = count_marked(player_map)
+        if revealed_amount == safe_amount:
+            game_win(size)
+            break
+
+        print()
         print_map(player_map)
-        print('Digite a coordenada para jogar, ex: =>A1\nAdicione -m para marcar uma bomba, ex: =>A1 -m\n')
+        print(f'Revelados: {revealed_amount}/{safe_amount}', end=" | ")
+        print(f'Marcados: {marked_amount}',  end=" | ")
+        print(f'Bombas: {bomb_amount}\n')
+        print('Digite a coordenada para jogar, ex: =>A1')
+        print('Adicione -m para marcar uma bomba, ex: =>A1 -m')
+        print('E para desistir, digite 0\n')
 
         # Pega a jogada do jogador e valida
         player_input = input("Sua jogada =>")
-        is_valid, coord, command = validate_input(player_input, valid_coords)
+        if player_input == "0":
+            game_over(game_map)
+            break
+        coord, command = validate_input(player_input, valid_coords)
 
         # Se a jogada for inválida, mostra mensagem de erro e pede outra jogada
-        if not is_valid:
+        if coord == None:
             print_invalid_input()
             continue
 
+        row, col = coord
         # Pega o tile dos dois mapas para comparação
-        game_tile = get_tile(game_map, coord)
-        player_tile = get_tile(player_map, coord)
+        game_tile = game_map[row][col]
+        player_tile = player_map[row][col]
 
-        # Verifica se o jogador marcou um tile ou se revelou um tile vazio
-        if command == "-m":
+        # Impede jogadas repetidas
+        if player_tile == game_tile:
+            print_invalid_input()
+            continue
+
+        # Verifica se o jogador marcou um tile ou se clicou em um tile
+        if command != None and "-m" in command:
             new_tile = mark_char if player_tile == empty_char else empty_char
         else:
             new_tile = game_tile
 
-        # Atualiza o mapa do jogador com o novo tile
+        # Computa a jogada
         if new_tile == bomb_char:
-            print("Você perdeu!")
-            print_map(game_map)
+            # Encerra o jogo
+            game_over(game_map)
             break
         elif new_tile == 0:
             # Revela os tiles ao redor do tile atual recursivamente
             player_map = reveal_tiles_around_zeros(player_map, game_map, coord)
         else:
-            player_map[coord[0]][coord[1]] = new_tile
+            # Apenas revela o tile escolhido
+            player_map[row][col] = new_tile
 
 
-def start_game(map_size, bomb_amount):
+def start_game(map_selected):
     print_separator()
     print("Carregando ... ", end="")
-    player_map = generate_map(map_size, char=empty_char)
-    game_map = generate_game_map(map_size, bomb_amount)
+    size = map_selected['size']
+    bomb_amount = map_selected['bomb_amount']
+    player_map = generate_map(size, char=empty_char)
+    game_map = generate_game_map(size, bomb_amount)
     print("pronto!")
     print_separator()
-    game_loop(player_map, game_map)
+    game_loop(player_map, game_map, bomb_amount)
 
 
 def main_menu():
     while True:
+        print()
         print_separator()
         print(f"{'':^7}Sweepy")
         print_separator(size=16, char=' -')
         print(f"{'':^5}1. Jogar")
         print(f"{'':^5}2. Ver Pontuações")
+        print()
         print(f"{'':^5}0. Sair")
         print_separator()
 
         choice = input("\nDigite sua escolha: ")
 
         if choice == "1":
-            map_size, bomb_amount = choose_size()
-            if map_size == None:
+            map_selected = choose_size()
+            if map_selected == None:
                 continue
             else:
-                start_game(map_size, bomb_amount)
+                start_game(map_selected)
         elif choice == "2":
             # code to view high scores goes here
             pass
